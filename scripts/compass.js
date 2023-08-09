aa6f66a26ce42aade414e7ae9a9f52e4cbcc59e7
Bruno Calado <brunocalado@users.noreply.github.com>
2023-08-09 11:17:30 -0300

    0.0.1

--- scripts/compass.js
+++ scripts/compass.js
@@ -43,8 +43,8 @@ export class Compass {
 
   static async moveThing(html) {
     let msg = '';
-    const newangle = parseInt( html.find("#newangle")[0].value );
-    movePattern1(newangle);  
+    const newAngle = parseInt( html.find("#newAngle")[0].value );
+    this.movePattern1(newAngle);  
   }
 
   /* ---------------------------------------------
@@ -53,28 +53,42 @@ export class Compass {
     compass.movePattern1(position, extraTime);
   */
   static async movePattern1(angle) {    
-
+      
+    // get current angle: pointer_token.document.rotation
+    const currentAngle = pointer_token.document.rotation;
+    const secondAngle = this.recalcuteAngle(currentAngle, 359);
+    const thirdAngle = this.recalcuteAngle(secondAngle, 180);
+    
+    console.log('====================')
+    console.log('====================')
+    console.log('====================')
+    console.log(currentAngle)
+    console.log(secondAngle)
+    console.log(thirdAngle)
+    
     let sequence = new Sequence()
       .animation()
-        .on(tokenD)
-        .rotateIn(0, 2500, {ease: "easeInOutBounce"})    // .rotateIn(degrees, duration, options)
+        .on(pointer_token)
+        .rotateIn(secondAngle, 2500)    // .rotateIn(degrees, duration, options)
         .waitUntilFinished()
-        .rotateIn(90, 1500, {ease: "easeInOutBounce"})    // .rotateIn(degrees, duration, options)
+        .rotateIn(thirdAngle, 1500)    // .rotateIn(degrees, duration, options)
         .waitUntilFinished()
-        .rotateIn(180, 2500, {ease: "easeInOutBounce"})    // .rotateIn(degrees, duration, options)
-        .waitUntilFinished()
-        .rotateIn(360, 2500, {ease: "easeInOutBounce"})    // .rotateIn(degrees, duration, options)
 
     await sequence.play();
     
     sequence = new Sequence()
       .animation()
-          .on(tokenD)
-          .rotateIn(angle, 2500, {ease: "easeOutCubic"})    // .rotateIn(degrees, duration, options)
+          .on(pointer_token)
+          .rotateIn(angle, 2500)    // .rotateIn(degrees, duration, options)
           .waitUntilFinished()
 
     await sequence.play();
   
+  }
+
+  static recalcuteAngle(angle, newAngleToAdd) {
+    const newAngle = (angle + newAngleToAdd) % 360;
+    return newAngle >= 0 ? newAngle : newAngle + 360;
   }
 
 } // CLASS END
--- scripts/init.js
+++ scripts/init.js
@@ -1,5 +1,5 @@
 const moduleName = 'compass-for-sequencer';
-//import {Compass} from './compass.js'
+import {Compass} from './compass.js'
 
 
 Hooks.once('init', () => {
@@ -8,13 +8,7 @@ Hooks.once('init', () => {
   // Load API
   // Request with: const compass = game.modules.get('compass-for-sequencer')?.api.compass;
   //game.modules.get('compass-for-sequencer').api = { compass }; 
-  //game.modules.get('compass-for-sequencer').api = { compass: Compass };
-  
-  console.log('==========================')
-  console.log('Compass',  {
-    module: game.modules.get('compass-for-sequencer')
-  });
-  console.log('==========================')
+  game.modules.get('compass-for-sequencer').api = { compass: Compass };
   
   // --------------------------------------------------
   // Module Options
